9e6bf704d0364a864e894fa2e8b1dea893cbdff2
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2019-09-19 20:11:56 -0700

    tas tools

---
+++ tastools.js
@@ -0,0 +1,14 @@
+var runner = new ActiveXObject("Wscript.Shell");
+runner.run('setup.sh')
+$('').keypress(function(event){
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if(keycode == '192'){
+    runner.run('mouseget.sh')
+  else if(keycode == '112'){
+    runner.run('kd.sh')
+  else if(keycode == '113'){
+    runner.run('ku.sh')
+  else if(keycode == '65'){
+    runner.run('a.sh')
+  else if(keycode == '113'){
+    runner.run('b.sh')
